64fbc5b29db62b1c3b6a653941f5537ce08c891a
c7w <cc7w@foxmail.com>
2024-02-23 22:18:36 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+set -e
+set -x
+
+### A reminder of how to deploy the course index to the remote `gitlab` repository
+# https://lab.cs.tsinghua.edu.cn/software-engineering/
+
+# if remote `gitlab` does not exist, create it and point to git@git.tsinghua.edu.cn:thuse-course/course-index.git
+if ! git remote | grep -q gitlab; then
+  git remote add gitlab git@git.tsinghua.edu.cn:thuse-course/course-index.git
+fi
+
+# push to remote `gitlab`
+git checkout gh-pages
+git pull origin gh-pages
+git push gitlab gh-pages
+git checkout master
